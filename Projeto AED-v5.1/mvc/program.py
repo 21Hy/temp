d94ef456801007d702a9978408fc675e3e2af730
Luís Raminhas <119435119+21Hy@users.noreply.github.com>
2023-05-25 15:12:31 +0100

    Delete Projeto AED-v5.1 directory

--- Projeto AED-v5.1/mvc/program.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from View import*
-
-
-if __name__ == '__main__':
-    root = tk.Tk()
-    app = View(root)
-    root.mainloop()
--- Projeto AED-v5.1/teste4.py
+++
@@ -1,3 +0,0 @@
-if True:print('Hello world')
-True
-False
